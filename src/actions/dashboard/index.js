cadd41741d586c690825ee1c74138f7255634623
Thulio Philipe <thulioph@gmail.com>
2018-04-04 16:00:08 -0300

    Adds the initial structure to work with Redux.

---
+++ src/actions/dashboard/index.js
@@ -0,0 +1,31 @@
+import { USER_LOGGED } from 'actions/dashboard/types';
+
+// ====
+
+const initialState = {
+    userIsLogged: false
+};
+
+// ====
+
+export const userLogged = (val) => ({
+    type: USER_LOGGED,
+    payload: val
+});
+
+// ====
+
+export default (state = initialState, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_LOGGED:
+            return {
+                ...state,
+                userIsLogged: payload
+            }
+    
+        default:
+            return state;
+    }
+}
