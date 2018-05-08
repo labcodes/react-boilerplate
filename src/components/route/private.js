0054a990b15239910e44e73f801a09033c2cff48
Thulio Philipe <thulioph@gmail.com>
2018-05-08 16:32:00 -0300

    Updates the PrivateRoute to be a Class.

--- src/components/route/private.js
+++ src/components/route/private.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Navbar from 'components/navbar';
+
+// ====
+
+class Private extends React.Component {
+    render() {
+        const { logged, component: Component, ...rest } = this.props;
+
+        if (!logged) {
+            return <Redirect to='/login' />;
+        }
+
+        return (
+            <main>
+                <Navbar />
+
+                <Route {...rest} render={(props) => (
+                    <Component {...props} />
+                )} />
+            </main>
+        );
+    }
+};
 
 // ====
 
