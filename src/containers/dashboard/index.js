718dff9eef4d50c4f20f473859a2c23589dbfce3
Thulio Philipe <thulioph@gmail.com>
2018-05-08 16:29:29 -0300

    Adds a new container called by Dashboard.

---
+++ src/containers/dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react';
+
+// ====
+
+const DashboardContainer = () => {
+    return (
+        <StrictMode>
+            <h1>Dashboard works!</h1>
+        </StrictMode>
+    );
+};
+
+// ====
+
+export default DashboardContainer;
--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -4,9 +4,11 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import PublicRoute from 'components/route/public';
+import PrivateRoute from 'components/route/private';
 
 import AppContainer from 'containers/app';
 import LoginContainer from 'containers/login';
+import DashComponent from 'containers/dashboard';
 import NotFoundContainer from 'containers/not-found';
 
 // ====
@@ -16,11 +18,10 @@ const Main = () => {
     return(
         <Router history={browserHistory}>
             <Switch>
-                {/* <Route path='/' exact render={() => (<Redirect to='/dashboard' />)} /> */}
-                {/* <PrivateRoute path='/dashboard' component={App} logged={userLogged} /> */}
-
                 <PublicRoute exact path='/' component={AppContainer} />
                 <PublicRoute path='/login' component={LoginContainer} />
+
+                <PrivateRoute path='/dashboard' component={DashComponent} />
 
                 <Route component={NotFoundContainer} />
             </Switch>
