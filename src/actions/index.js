6174bd19fd6de1a824309806fe72fb24bfdadc91
Thulio Philipe <thulioph@gmail.com>
2018-05-15 15:32:15 -0300

    Adds new actions to work with login container.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
 import system from './system';
+import login from './login';
 
 // ====
 
 export default combineReducers({
     system,
+    login,
     form
 });
