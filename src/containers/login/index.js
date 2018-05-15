2dcc6d66ac13aa6e620bfe348f002a3eb852361b
Thulio Philipe <thulioph@gmail.com>
2018-05-15 15:08:18 -0300

    Updates the login container with Form component.

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,14 +1,28 @@
 import React, { StrictMode } from 'react';
+import FormComponent from 'components/form';
 
 // ====
 
-const LoginContainer = () => {
-    return (
-        <StrictMode>
-            <h1>Login!</h1>
-        </StrictMode>
-    );
+class LoginContainer extends React.Component {
+    handleSubmitForm(values) {
+        // const { addFormContent } = this.props;
+        // addFormContent(values);
+        console.warn('Submiting form using [LoginContainer]', values);
+    }
+
+    render(){
+        return (
+            <StrictMode>
+                <section>
+                    <h1>Login!</h1>
+
+                    <FormComponent submitForm={this.handleSubmitForm.bind(this)} />
+                </section>
+            </StrictMode>
+        );
+    }
 };
+
 
 // ====
 
