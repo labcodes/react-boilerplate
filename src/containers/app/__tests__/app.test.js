3f26182192c6630f5ba8edd94d63fac49416c8ca
Thulio Philipe <thulioph@gmail.com>
2018-10-02 19:01:45 -0300

    Updates the snap file.

--- src/containers/app/__tests__/app.test.js
+++ src/containers/app/__tests__/app.test.js
@@ -1,6 +1,5 @@
-'use strict';
-
 import React from 'react';
+import { shallow } from 'enzyme';
 
 // Test helpers
 import renderer from 'react-test-renderer';
@@ -13,19 +12,30 @@ import store from 'store';
 // Component
 import App from 'containers/app';
 
-// ====
-
 describe('App [Snapshot]', () => {
-    it('Renders correctly', () => {
-        const component = (
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </BrowserRouter>
-        );
+  it('Renders correctly', () => {
+    const component = (
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    );
 
-        const tree = renderer.create(component).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Should render the message [User is logged].', () => {
+    const component = shallow(
+      <BrowserRouter>
+        <Provider store={store}>
+          <App logged={false} />
+        </Provider>
+      </BrowserRouter>
+    );
+    // const paragraph = component.children().find('p');
+    // console.warn(paragraph.text())
+    // expect(paragraph.text()).toEqual('User is logged');
+  });
 });
